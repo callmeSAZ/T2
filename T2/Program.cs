0977d3a6c39190873d554b271b56e1fe3fbc5c43
callmeSAZ <121518844+callmeSAZ@users.noreply.github.com>
2023-03-06 20:03:23 +0330

    Add project files.

---
+++ T2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //دریافت ورودی و چاپ عدد اسکی
+            char ch = char.Parse(Console.ReadLine());
+            int x = (int)ch;   
+            Console.WriteLine(x);
+
+            Console.ReadKey();
+        }
+    }
+}
